13d6b93f042339a0e754c3f341d25180ebcef10f
@Ronnavelly <ronnavelly@gmail.com>
2018-09-07 16:30:41 +0700

    Initial commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,11 @@
+public class Dosen {
+	String namaDosen = "";
+	
+	public String getNamaDosen() {
+		return namaDosen;
+	}
+	
+	public void setNamaDosen(String nama) {
+		this.namaDosen = nama;
+	}
+}
